e0186e2ae7974ec05d930c9414927fd2c92c2077
pedrozampier <pedrootaviopz@gmail.com>
2024-09-27 09:12:36 -0300

    refactor: Update Table.vue to use dynamic headers and labels for the table

--- resources/js/Components/Table.vue
+++ resources/js/Components/Table.vue
@@ -3,16 +3,16 @@
         <table class="min-w-full bg-white border border-gray-200">
             <thead>
                 <tr class="bg-gray-100 text-left">
-                    <th class="py-2 px-4 border-b">Temperatura</th>
-                    <th class="py-2 px-4 border-b">Eficiência</th>
-                    <th class="py-2 px-4 border-b">Data e hora</th>
+                    <th v-for="(header, index) in props.labels" :key="index" class="py-2 px-4 border-b">
+                        {{ header }}
+                    </th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="(row, index) in combinedData" :key="index" class="hover:bg-gray-50">
-                    <td class="py-2 px-4 border-b">{{ row.temperature }}</td>
-                    <td class="py-2 px-4 border-b">{{ row.efficiency }}</td>
-                    <td class="py-2 px-4 border-b">{{ row.recorded_at }}</td>
+                <tr v-for="(row, rowIndex) in tableData" :key="rowIndex" class="hover:bg-gray-50">
+                    <td v-for="(header, colIndex) in props.headers" :key="colIndex" class="py-2 px-4 border-b">
+                        {{ row[header] }}
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -20,36 +20,38 @@
 </template>
 
 <script setup>
-import { defineProps, onBeforeMount, ref, watch } from 'vue';
+import { defineProps, ref, onMounted, watch } from 'vue';
 import axios from 'axios';
 
 const props = defineProps({
     endpoint: {
         type: String,
         required: true
+    },
+    headers: {
+        type: Array,
+        required: true
+    },
+    labels: {
+        type: Array,
+        required: true
     }
 });
 
-const combinedData = ref([]);
+const tableData = ref([]);
 
 const getData = async () => {
     try {
         const response = await axios.get(props.endpoint);
 
-        const { temperature, efficiency, recorded_at } = response.data;
-
-        combinedData.value = temperature.map((temp, index) => ({
-            temperature: temp,
-            efficiency: efficiency[index],
-            recorded_at: recorded_at[index]
-        }));
+        tableData.value = response.data;
 
     } catch (error) {
-        console.error(error);
+        console.error('Erro ao buscar dados:', error);
     }
 };
 
-onBeforeMount(() => {
+onMounted(() => {
     getData();
 });
 
